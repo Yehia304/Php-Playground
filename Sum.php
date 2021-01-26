1fded43913981be1ddd8357bc008d2128f932b72
Yehia El Sayed <yehiafml20333@gmail.com>
2021-01-26 08:22:30 +0200

    commit

---
+++ Sum.php
@@ -0,0 +1,44 @@
+<?php
+
+$yehia = 'yehiaelsayedibrahim';
+
+$vowels = array('a','e','i','o','u');
+
+$new = str_replace($vowels, array_map('strtoupper', $vowels), $yehia);
+
+echo $new;
+
+
+$string = 'some string';
+
+$split = str_split($string);
+
+foreach ($split as $key => $value) {
+	if ($split[$key] == 'i' or $split[$key] == 'o' or $split[$key] == 'a' or $split[$key] == 'e' or $split[$key] =='u') {
+		$split[$key] = strtoupper($split[$key]);
+	}
+}
+echo "<br>";
+echo implode($split);
+
+foreach ($color as $value) {
+
+	//echo $value;
+	echo "<li>$value</li>";
+}
+echo "</ul>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ?>
--- training.php
+++ training.php
@@ -1,64 +1,25 @@
 <?php
+$new = "11Yehia 8Mohab";
+$str = "";
+$arr = array();
+$split = str_split($new);
+foreach ($split as $key => $value){
+    if (is_numeric($value)){
 
-$file = 'Directory';
+        $str = $str . $value;
 
-echo __DIR__;
+    }
 
-if(is_dir(__DIR__ . "/". $file ) ){
+    if(!is_numeric($split[$key+1])){
 
-echo "Exists";
+        array_push($arr,$str);
+
+        $str = "";
+
+    }
+
 }
 
-mkdir($file);
-echo "<br> DOESNT EXIT but i already created it.";
-$newdir = __DIR__ . "/" . $file;
-echo "<br>" . $newdir;
+echo $str;
 
-
-//file_put_contents($newdir . '/Php.txt', "Yehia is HUGE");
-
-//$mahmod = file_get_contents($newdir . '/Php.txt');
-
-//echo "<br>". $mahmod;
-
-//chmod($newdir . "/Php.txt", 0444);
-
-if(is_writable($newdir . "/Php.txt")){
-
-	echo "<br> Writeable";
-}
-
-echo "<br> File is not even writable";
-$directory = $newdir . "/Php.txt";
-echo "<br>" . $directory;
-copy($newdir . "/Php.txt", $newdir . "/medhat.txt" );
-
-$array = pathinfo(__DIR__);
-
-echo "<pre>";
-print_r($array);
-echo "</pre>";
-
-echo "<br>";
-
-$wha2 = scandir($array['dirname']);
-//echo "<pre>";
-//print_r($wha2);
-//echo "</pre>";
-
-if(array_search('asdasd', $wha2)){
-	echo "<br> A7A";
-}
-else{
-	echo "HAHAHAHAHAHAHAHAH";
-}
-
-$filegot = fopen($newdir . "/Php.txt", 'w');
-//$content = fread($filegot, filesize($newdir . "/Php.txt"));
-//echo "<br>" . $content;
-fwrite($filegot, "Merna is lovely.");
-echo "<br> Handled";
-$written = file_get_contents($directory);
-echo "<br>" . $written;
-fclose($filegot);
 ?>
