abb1c3c0d3e6dbbd9fa03d5716ea952d981a8ffb
Yehia El Sayed <yehiafml20333@gmail.com>
2021-02-01 13:45:49 +0200

    Push zeros to the end of the array

---
+++ Pushzeros.php
@@ -0,0 +1,35 @@
+<?php 
+
+function pushzeros($val){
+
+
+foreach ($val as $key => $value) {
+	if ($value == 0) {
+		unset($val[$key]);
+		array_push($val, 0);
+	}
+}
+
+print_r($val);
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+$zeros = array(0,0,0,1,2,3,4,5);
+
+pushzeros($zeros);
+
+
+
+
+?>
