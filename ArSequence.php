984bdb60fd156725bdff52784548858fa397ea89
Yehia El Sayed <yehiafml20333@gmail.com>
2021-02-02 09:35:07 +0200

    Arithmatic sequence

--- ArSequence.php
+++ ArSequence.php
@@ -4,25 +4,31 @@
 function CheckArSequence($arr){
 
 $new = $arr[1] - $arr[0];
-echo $new;
+$count = 0 ;
+
 
 foreach ($arr as $key => $value) {
-	$diff = $arr[$key+1] - $value;
+	$val2 = current($arr);
+	$val1 = next($arr);
+	
+	$diff = $val1 - $val2;
+	
+	echo "val2 : " . $val2 . "<br>";
+	echo "val1 : " . $val1 . "<br>";
 
 	if($diff != $new){
 
-		echo "Not arithmatic sequence";
+		$count = $count + 1;
+	}
+}
+
+if ($count == 1) {
+		echo "It is an arithmatic sequence";
 	}
 
 	else {
-		echo "Arithmatic sequence keep going" . "<br>";
+		echo "It is not an arithmatic sequence";
 	}
-	
-
-
-
-
-}
 
 
 
