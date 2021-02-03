5b1b6c1bcb1af0bc022404a9457b61a79dbdd15e
Yehia El Sayed <yehiafml20333@gmail.com>
2021-02-03 13:33:11 +0200

    Anagram

---
+++ Anagram.php
@@ -0,0 +1,29 @@
+<?php 
+
+
+function Anagram($arr1,$arr2){
+
+if (count_chars($arr1,1) == count_chars($arr2,1)) {
+	echo 'It is an anagram';
+}
+
+else {
+
+	echo "Not an anagram";
+
+
+}
+}
+
+
+$arr1 = 'anagram';
+$arr2 = 'nagaram';
+
+
+Anagram($arr1,$arr2);
+
+
+
+
+
+ ?>
