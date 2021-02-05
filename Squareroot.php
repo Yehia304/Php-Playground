83d670704733410fb99174aa6e2a23c745118bb7
Yehia El Sayed <yehiafml20333@gmail.com>
2021-02-05 13:30:13 +0200

    Easy square root

---
+++ Squareroot.php
@@ -0,0 +1,47 @@
+<?php 
+
+function squareroot($ip){
+
+
+
+
+	return $ip * $ip;
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+$ip = 3;
+
+
+
+
+$sqr = squareroot($ip);
+echo $sqr;
+
+
+
+
+
+
+
+
+
+ ?>
